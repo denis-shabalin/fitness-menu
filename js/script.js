4695a904bb08e6b78b99ba100a4f2807d4280af9
Denis Shabalin <shabalin.denis03@mail.ru>
2023-11-13 23:50:40 +0300

    fixed the countdown timer

--- js/script.js
+++ js/script.js
@@ -42,14 +42,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
    // Timer
 
-   const deadline = '2023-11-14';
+   const deadline = '2023-11-13';
 
    function getTimeRemaining(endtime) {
-      const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+      let days, hours, minutes, seconds;
+      const t = Date.parse(endtime) - Date.parse(new Date());
+
+      if (t <= 0) {
+         days = 0;
+         hours = 0;
+         minutes = 0;
+         seconds = 0;
+      } else {
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+         minutes = Math.floor((t / 1000 / 60) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+      }
 
       return {
          'total': t,
@@ -88,10 +97,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
          if (t.total <= 0) {
             clearInterval(timeInterval);
-            days.innerHTML = getZero(0);
-            hours.innerHTML = getZero(0);
-            minutes.innerHTML = getZero(0);
-            seconds.innerHTML = getZero(0);
          }
       }
    }
