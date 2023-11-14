dcf9391aa0363aeae60801f8083be8be3dce5125
Denis Shabalin <shabalin.denis03@mail.ru>
2023-11-14 22:03:12 +0300

    with the help of classes, i set up menu cards for the day

--- js/script.js
+++ js/script.js
@@ -146,7 +146,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    });
 
-   const modalTimerId = setTimeout(openModal, 5000);
+   //const modalTimerId = setTimeout(openModal, 5000);
 
    function showModalByScroll() {
       if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -157,45 +157,65 @@ window.addEventListener('DOMContentLoaded', () => {
 
    window.addEventListener('scroll', showModalByScroll);
 
-   // Forms
+   // Используем классы для карточек
+   class MenuCard {
+      constructor(src, alt, title, descr, price, parentSelector) {
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.descr = descr;
+         this.price = price;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27; //курс валюты
+         this.changeToUAN();
+      }
 
-   const forms = document.querySelectorAll('form');
+      changeToUAN() {
+         this.price = this.price * this.transfer;
+      }
 
-   const message = {
-      loading: 'Загрузка',
-      success: 'Спасибо',
-      failure: 'Что то пошло не так'
-   };
-
-   forms.forEach(item => {
-      postData(item);
-   });
-
-   function postData(form) {
-      form.addEventListener('submit', (e) => {
-         e.preventDefault();
-
-         const statusMessage = document.createElement('div');
-         statusMessage.classList.add('status');
-         statusMessage.textContent = message.loading;
-         form.append(statusMessage);
-
-         const request = new XMLHttpRequest();
-         request.open('POST', 'server.php');
-
-         request.setRequestHeader('Content-type', 'multipart/form-data');
-         const formData = new FormData(form);
-
-         request.send(formData);
-
-         request.addEventListener('load', () => {
-            if (request.status === 200) {
-               console.log(request.response);
-               statusMessage.textContent = message.success;
-            } else {
-               statusMessage.textContent = message.failure;
-            }
-         });
-      });
+      render() { // формирование верстки
+         const element = document.createElement('div');
+         element.innerHTML = `
+            <div class="menu__item">
+               <img src=${this.src} alt=${this.alt}>
+               <h3 class="menu__item-subtitle">${this.title}</h3>
+               <div class="menu__item-descr">${this.descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span></span> грн/день</div>
+               </div>
+            </div>
+         `;
+         this.parent.append(element);
+      }
    }
+
+   new MenuCard(
+      "img/tabs/vegy.jpg",
+      "vegy",
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      9,
+      '.menu .container'
+   ).render(); // метод отработает 1 раз и удалится, так как не записывает в переменную(нет ссылки)
+
+   new MenuCard(
+      "img/tabs/elite.jpg",
+      "elite",
+      'Меню “Премиум”',
+      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+      21,
+      '.menu .container'
+   ).render();
+
+   new MenuCard(
+      "img/tabs/post.jpg",
+      "post",
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      14,
+      '.menu .container'
+   ).render();
 });
